8cb6d6b2824b9ef9b0a8e2e92b67d494420a4597
hirunanimesh <158060469+hirunanimesh@users.noreply.github.com>
2025-01-11 20:43:56 +0530

    Delete src directory

--- src/TypeNotSupportedException.java
+++
@@ -1,5 +0,0 @@
-public class TypeNotSupportedException extends Exception {
-    public TypeNotSupportedException(String message) {
-        super(message); // Correctly pass the message to the superclass constructor
-    }
-}
